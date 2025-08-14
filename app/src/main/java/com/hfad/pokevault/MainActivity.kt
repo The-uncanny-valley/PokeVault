921e2681d2afd222a18e332ca4a5c184eb442802
the-uncanny-valley <runcannyvalley@gmail.com>
2025-08-14 13:49:29 +0300

    Load PokemonListFragment on MainActivity launch

--- app/src/main/java/com/hfad/pokevault/MainActivity.kt
+++ app/src/main/java/com/hfad/pokevault/MainActivity.kt
@@ -3,18 +3,19 @@ package com.hfad.pokevault
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.hfad.pokevault.presentation.ui.PokemonListFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, PokemonListFragment())
+                .commit()
         }
     }
 }
