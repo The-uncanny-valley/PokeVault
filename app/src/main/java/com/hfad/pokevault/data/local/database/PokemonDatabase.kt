4b9cbdd6bdc8eb0bb2a5963a81312f4d76b04c7c
the-uncanny-valley <runcannyvalley@gmail.com>
2025-09-27 19:55:36 +0300

    refactor(data): reorganize structure into dao, database and entity folders

--- app/src/main/java/com/hfad/pokevault/data/local/database/PokemonDatabase.kt
+++ app/src/main/java/com/hfad/pokevault/data/local/database/PokemonDatabase.kt
@@ -1,8 +1,11 @@
-package com.hfad.pokevault.data.db
+package com.hfad.pokevault.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.hfad.pokevault.data.local.dao.PokemonDao
+import com.hfad.pokevault.data.local.entity.PokemonEntity
+import com.hfad.pokevault.data.local.entity.PokemonTypeEntity
 
 @Database(
     entities = [PokemonEntity::class, PokemonTypeEntity::class],
